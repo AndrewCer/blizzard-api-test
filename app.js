aaea3a1d33348983b6d76df7ef7b076ec85257bc
Andrew Cer <cer.andrew@gmail.com>
2015-09-17 11:17:00 -0600

    init bnet request setup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 require('dotenv').load();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +23,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var BnetStrategy = require('passport-bnet').Strategy;
+var BNET_ID = process.env.BNET_ID
+var BNET_SECRET = process.env.BNET_SECRET
+
+// Use the BnetStrategy within Passport.
+passport.use(new BnetStrategy({
+    clientID: BNET_ID,
+    clientSecret: BNET_SECRET,
+    callbackURL: "https://localhost:3000/auth/bnet/callback"
+}, function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+app.get('/auth/bnet',passport.authenticate('bnet'));
+app.get('/auth/bnet/callback',
+    passport.authenticate('bnet', { failureRedirect: '/' }),
+    function(req, res){
+        res.redirect('/');
+    });
 
 app.use('/', routes);
 app.use('/users', users);
